6c7ede6adbdc5786995b9f319926fb3aafa804dd
Harold "Harry" Umali <hsumali@up.edu.ph>
2016-12-24 14:53:38 +0800

    Added modulus function

--- user_math.h
+++ user_math.h
@@ -10,3 +10,8 @@ void divideTwoNumber(int num1, int num2, float *quo){
 	if(num2==0) printf("INVALID DIVISION!\n");
 	else *quo = num1 / num2; 
 }
+
+void modulusTwoNumber(int num1, int num2, float *quo){ 
+	if(num2==0) printf("INVALID DIVISION!\n");
+	else *quo = num1 % num2; 
+}
