b5fd9c1a3658f9d42c31b64be94d650f93c19194
Harold "Harry" Umali <hsumali@up.edu.ph>
2016-12-24 14:37:55 +0800

    Create user_math.h
    Header file for my user-defined mathematical statements

---
+++ user_math.h
@@ -0,0 +1,12 @@
+
+
+void addTwoNumber(int num1, int num2, int *sum){ *sum = num1 + num2; }
+
+void minusTwoNumber(int num1, int num2, int *dif){ *dif = num1 - num2; }
+
+void multiplyTwoNumber(int num1, int num2, int *prod){ *prod = num1 * num2; }
+
+void divideTwoNumber(int num1, int num2, float *quo){ 
+	if(num2==0) printf("INVALID DIVISION!\n");
+	else *quo = num1 / num2; 
+}
